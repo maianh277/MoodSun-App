d97da3a2903ddfb794aa1503272480d539c25456
MaiAnh <dotranmaianh2002@gmail.com>
2023-11-16 14:58:21 +0700

    done Hompage

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,17 +1,19 @@
-import { View, Text, Image } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import CustomCalendar from "../components/CustomCalendar";
 
+import EmotionEachDay from "../components/EmotionEachDay";
+import EmotionPick from "../components/EmotionPick";
 export default function HomePage() {
   return (
     <View>
-      <CustomCalendar />
-      <Text style={tw`ml-5 mt-8 font-bold text-xl`}>Today</Text>
       <View>
-        <Text>How was your day?</Text>
-        <Image source={require("../assets/emoji/happy.png")} />
+        <CustomCalendar />
       </View>
+      <Text style={tw`ml-6 font-bold text-xl`}>Today</Text>
+      <EmotionEachDay />
+      <EmotionPick x />
     </View>
   );
 }

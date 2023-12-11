aba27494ee413f2e687e3fedef3c1f41e404e96a
MaiAnh <dotranmaianh2002@gmail.com>
2023-12-11 14:45:58 +0700

    logout

--- screens/Setting.js
+++ screens/Setting.js
@@ -1,14 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import tw from "twrnc";
 import Account from "../components/Account";
 import Customization from "../components/Customization";
 import ServiceCenter from "../components/ServiceCenter";
 import { useNavigation } from "@react-navigation/native";
+import { auth } from "../config/FirebaseConfig";
+
 const SettingsScreen = () => {
   const [isEnabled, setIsEnabled] = useState(false);
   const navigation = useNavigation();
+  const [email, setEmail] = useState("");
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setEmail(user.email);
+      } else {
+        setEmail(null);
+        navigation.replace("Login");
+      }
+    });
+    return unsubscribe;
+  }, []);
+  const handleLogout = () => {
+    console.log("Logging out...");
+    auth
+      .signOut()
+      .then(
+        () => console.log("Sign out successful"),
+        navigation.navigate("Login")
+      )
+      .catch((error) => {
+        console.error("Error signing out: ", error.code, error.message);
+      });
+  };
 
   return (
     <View style={tw`bg-white p-7 pt-10 flex-1`}>
@@ -16,7 +42,7 @@ const SettingsScreen = () => {
       <Account />
       <Customization isEnabled={isEnabled} toggleSwitch={toggleSwitch} />
       <ServiceCenter />
-      <Text onPress={() => navigation.navigate("Login")}>Log out</Text>
+      {email ? <Text onPress={handleLogout}>Log out</Text> : ""}
     </View>
   );
 };
